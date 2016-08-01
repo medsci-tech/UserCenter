8aa1930f65b1123800e6d4b5d92ead062fbd6127
mime <zhaiyu_hb@hotmail.com>
2016-08-01 13:48:40 +0800

    添加ui模板common公共html文件

--- UserCenter/urls.py
+++ UserCenter/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import *
 from users import temp
 urlpatterns = patterns(
-    url(r'^/$', temp.index),
     url(r'^temp/index', temp.index),
     url(r'^temp/calendar', temp.calendar),
     url(r'^temp/tables', temp.tables),
     url(r'^temp/', temp.index),
+    url(r'', temp.login),
 )
