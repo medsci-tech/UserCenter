79a33b917ed915a7af3412457ed5322b5e1cb3ec
mime <zhaiyu_hb@hotmail.com>
2016-08-08 17:37:07 +0800

    修改配置

--- UserCenter/urls.py
+++ UserCenter/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import *
-from users import temp,tests
+from users.controller import manager
+from users import temp, tests  # test
 urlpatterns = patterns(
+
+    url(r'^manager/list', manager.index),
+    url(r'^manager', manager.index),
+
+    # test
     url(r'^temp/index', temp.index),
     url(r'^temp/tables', temp.tables),
-    url(r'^temp/testdb', temp.testdb),
-    url(r'^temp/', temp.index),
-
+    # url(r'^temp/testdb', temp.testdb),
+    # url(r'^temp/', temp.index),
     url(r'', temp.login),
 )
