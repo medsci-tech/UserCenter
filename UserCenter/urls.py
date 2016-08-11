f28378128fc2f8c281003603acbe419ff6075e48
mime <zhaiyu_hb@hotmail.com>
2016-08-11 12:23:30 +0800

    管理员

--- UserCenter/urls.py
+++ UserCenter/urls.py
@@ -1,15 +1,17 @@
 from django.conf.urls import *
-from users.controller import manager
+from users.controller import admin
 from users import temp, tests  # test
 urlpatterns = patterns(
-
-    url(r'^manager/list', manager.index),
-    url(r'^manager', manager.index),
-
+    '',
+    # 管理员管理
+    url(r'^admin/add', admin.add),
+    url(r'^admin/form', admin.form),
+    url(r'^admin/ajax', admin.form),
+    url(r'^admin/', admin.index),
     # test
     url(r'^temp/index', temp.index),
     url(r'^temp/tables', temp.tables),
     # url(r'^temp/testdb', temp.testdb),
     # url(r'^temp/', temp.index),
-    url(r'', temp.login),
+    # url(r'', temp.login),
 )
--- users/controller/admin.py
+++ users/controller/admin.py
@@ -4,19 +4,17 @@
 from django.shortcuts import render
 from users.model.Admin import Admin
 from django.http import HttpResponse
-
+from django.contrib.auth.hashers import make_password
 from django.core.paginator import Paginator
 from django.core.paginator import EmptyPage
 from django.core.paginator import PageNotAnInteger
 
+
 def index(request):
     param = {'username':'zhaiyu'}
     data = Admin().find(**param)
-    # topics = data.count()
-    limit = 3  # 每页显示的记录数
-
+    limit = 20  # 每页显示的记录数
     paginator = Paginator(data, limit)  # 实例化一个分页对象
-
     page = request.GET.get('page')  # 获取页码
     try:
         topics = paginator.page(page)  # 获取某页对应的记录
@@ -25,12 +23,26 @@ def index(request):
     except EmptyPage:  # 如果页码太大，没有相应的记录
         topics = paginator.page(paginator.num_pages)  # 取最后一页的记录
 
-    return render(request, 'manager/index.html',{'dataList':data,'dataPage':topics})
+    return render(request, 'admin/index.html',{'topics':topics})
 
 def add(request):
-    param = {'username': 'zhaiyu','password':123456,'email':'123@123.com','status':1}
+    param = {
+        'username': 'zhaiyu',
+        'password':make_password('123456',None,'pbkdf2_sha256'),
+        'email':'123@123.com',
+        'status':1,
+    }
     # param = (('username' , 'zhaiyu'),('password' , '123456'))
     # param = {'username','zhaiyu','passwor'}
     res = Admin(**param).add(**param)
+
     return HttpResponse(res)
-    # return render(request, 'manager/index.html')
+    # return render(request, 'admin/index.html')
+
+#
+def form(request):
+    pass
+
+# 修改操作
+def ajax(request):
+    pass
