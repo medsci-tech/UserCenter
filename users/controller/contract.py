bcae1f621da544d30fbc0f7a09c0192ee223d8dc
mime <zhaiyu_hb@hotmail.com>
2016-08-23 15:47:17 +0800

    合同管理

--- users/controller/contract.py
+++ users/controller/contract.py
@@ -2,9 +2,8 @@
 # 管理员管理
 # zhaiyu
 from django.shortcuts import render
-from users.model.Admin import Admin
+from users.model.Contract import Contract
 from django.http import HttpResponse
-from django.contrib.auth.hashers import make_password
 from django.core.paginator import Paginator
 from django.core.paginator import EmptyPage
 from django.core.paginator import PageNotAnInteger
@@ -16,16 +15,13 @@ def index(request):
     post = request.POST
     param = {}
     if request.method == "POST":
-        nickname = post.get('nickname')
-        username = post.get('username')
-        email = post.get('email')
-        if nickname:
-            param.update(nickname={'$regex': nickname})
-        if username:
-            param.update(username={'$regex': username})
-        if email:
-            param.update(email={'$regex': email})
-    data = Admin().find(**param)
+        name = post.get('name')
+        number = post.get('number')
+        if name:
+            param.update(name={'$regex': name})
+        if number:
+            param.update(number={'$regex': number})
+    data = Contract().find(**param)
     limit = 20  # 每页显示的记录数
     paginator = Paginator(data, limit)  # 实例化一个分页对象
     page = request.GET.get('page')  # 获取页码
@@ -36,19 +32,14 @@ def index(request):
     except EmptyPage:  # 如果页码太大，没有相应的记录
         topics = paginator.page(paginator.num_pages)  # 取最后一页的记录
 
-    return render(request, 'admin/index.html',{'topics':topics, 'request': post})
+    return render(request, 'contract/index.html',{'topics':topics, 'request': post})
 
 # 添加操作--protected
 def _add(**param):
     id = param.get('id')
-    password = param.get('password')
     if not id:
-        if not password:
-            # 如果没有填密码则为默认密码
-            password = '123456'
-        param.update(password=make_password(password, None, 'pbkdf2_sha256'))
         try:
-            model = Admin(**param).add(**param)
+            model = Contract(**param).add(**param)
             if model:
                 returnData = {'code': '200', 'msg': '操作成功', 'data': str(model)}
             else:
@@ -62,16 +53,13 @@ def _add(**param):
 # 修改操作--protected
 def _editById(**param):
     id = param.get('id')
-    password = param.get('password')
+    img = param.get('img')
     if id:
-        if hasattr(param, 'password'):
-            if password:
-                param.update(password=make_password(password, None, 'pbkdf2_sha256'))
-            else:
-                # 如果留空则移除password属性，不修改密码
-                param.pop('password')
+        if not img:
+            # 如果留空则移除img属性，不做修改
+            param.pop('img')
         try:
-            model = Admin(**param).editById(**param)
+            model = Contract(**param).editById(**param)
             '''
                 返回值 model
                 nModified:修改成功1，修改失败0
@@ -98,11 +86,12 @@ def form(request):
     post = request.POST
     id = post.get('id')
     param = {
-        'username': post.get('username'),
-        'nickname': post.get('nickname'),
-        'password': post.get('password'),
-        'email': post.get('email'),
-        'status': post.get('status'),
+        'name': post.get('name'),
+        'number': post.get('number'),
+        'amount': post.get('amount'),
+        'img': post.get('img'),
+        'startTime': post.get('startTime'),
+        'endTime': post.get('endTime'),
     }
     if id:
         # 修改
@@ -129,7 +118,7 @@ def stats(request):
     }
 
     try:
-        model = Admin(**param).editByIds(selection, **param)
+        model = Contract(**param).editByIds(selection, **param)
         if model.get('n'):
             if model.get('ok'):
                 returnData = {'code': '200', 'msg': '操作成功', 'data': ''}
