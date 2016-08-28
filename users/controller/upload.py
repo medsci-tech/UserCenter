333bf7d1ef52aa6560c8740b8e46ef0f2f9bdf81
mime <zhaiyu_hb@hotmail.com>
2016-08-28 21:45:50 +0800

    文件上传

--- users/controller/upload.py
+++ users/controller/upload.py
@@ -17,17 +17,22 @@ def handle_uploaded_file (filepath, file):
     time_now = datetime.now()
     pattern = re.compile(r'.[A-Za-z]*$')
     suffix = pattern.findall(file.name)
-    save_path = '%s/%s' %(filepath, time_now.strftime('%Y%m'))
+    save_path = '%s/%s' % (filepath, time_now.strftime('%Y%m'))
     save_name = '%s%s%s' % (time.time(), random.randint(1000, 9999), str(suffix[0]))
     path = '%s/%s/' % (UPLOAD_ROOT, save_path)
     if not os.path.exists(path):
         os.makedirs(path)
     f = path + save_name
     fd = open(f, 'wb+')
-    for chunk in file.chunks():
-        fd.write(chunk)
-    fd.close()
-    return '%s/%s' % (save_path, save_name)
+    try:
+        for chunk in file.chunks():
+            fd.write(chunk)
+        returnData = {'code': '200', 'msg': '成功', 'data': '/%s/%s' % (save_path, save_name)}
+    except Exception:
+        returnData = {'code': '900', 'msg': '失败', 'data': ''}
+    finally:
+        fd.close()
+    return returnData
 
 # 上传图片
 @csrf_exempt
@@ -38,11 +43,14 @@ def img(request):
     file_type = cfg_param.get('c_img_type')
     if file.content_type in file_type:
         try:
-            filename = handle_uploaded_file(file_path, file)
-            returnData = {'code': '200', 'msg': '操作成功', 'data': filename}
+            save_name = handle_uploaded_file(file_path, file)
+            if save_name.get('code') == '200':
+                returnData = {'code': '200', 'msg': '成功', 'data': {'tName': file.name, 'saveName': [save_name.get('data')]}}
+            else:
+                returnData = {'code': '802', 'msg': '失败', 'data': ''}
         except Exception:
-            returnData = {'code': '900', 'msg': '', 'data': ''}
+            returnData = {'code': '900', 'msg': '失败', 'data': ''}
     else:
-        returnData = {'code': '200', 'msg': '文件格式不支持', 'data': ''}
+        returnData = {'code': '801', 'msg': '文件格式不支持', 'data': ''}
 
     return HttpResponse(json.dumps(returnData), content_type="application/json")
