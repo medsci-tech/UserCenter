9c08a0524cdad430d2dd96bb7a5297dc0d096436
mime <zhaiyu_hb@hotmail.com>
2016-08-29 16:43:06 +0800

    合同管理-添加日期插件

--- users/static/js/jquery-ui.custom.js
+++ users/static/js/jquery-ui.custom.js
@@ -6841,12 +6841,18 @@ function Datepicker() {
 		prevText: "Prev", // Display text for previous month link
 		nextText: "Next", // Display text for next month link
 		currentText: "Today", // Display text for current month link
-		monthNames: ["January","February","March","April","May","June",
-			"July","August","September","October","November","December"], // Names of months for drop-down and formatting
-		monthNamesShort: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"], // For formatting
-		dayNames: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"], // For formatting
-		dayNamesShort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"], // For formatting
-		dayNamesMin: ["Su","Mo","Tu","We","Th","Fr","Sa"], // Column headings for days starting at Sunday
+		// monthNames: ["January","February","March","April","May","June",
+		// 	"July","August","September","October","November","December"], // Names of months for drop-down and formatting
+		// monthNamesShort: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"], // For formatting
+		// dayNames: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"], // For formatting
+		// dayNamesShort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"], // For formatting
+		// dayNamesMin: ["Su","Mo","Tu","We","Th","Fr","Sa"], // Column headings for days starting at Sunday
+		monthNames: ["一月","二月","三月","四月","五月","六月","七月","八月","九月","十月","十一月","十二月"],
+		monthNamesShort: ["一","二","三","四","五","六","七","八","九","十","十一","十二"],
+		dayNames: ["星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日"],
+		dayNamesShort: ["一","二","三","四","五","六","日"],
+		dayNamesMin: ["一","二","三","四","五","六","日"],
+
 		weekHeader: "Wk", // Column header for week of the year
 		dateFormat: "mm/dd/yy", // See format options on parseDate
 		firstDay: 0, // The first day of the week, Sun = 0, Mon = 1, ...
