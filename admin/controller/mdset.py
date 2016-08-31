63d407965f2df1541e4dfcffa443044730de56c7
zhaiyu <zhaiyu_hb@hotmail.com>
2016-08-31 12:19:05 +0800

    添加迈豆积分配置

---
+++ admin/controller/mdset.py
@@ -0,0 +1,145 @@
+#_*_coding:utf-8_*_
+# 管理员管理
+
+from django.shortcuts import render
+from admin.model.Mdset import Mdset
+from django.http import HttpResponse
+from django.contrib.auth.hashers import make_password
+from django.core.paginator import Paginator
+from django.core.paginator import EmptyPage
+from django.core.paginator import PageNotAnInteger
+from django.views.decorators.csrf import csrf_exempt
+import json
+
+'''
+管理员列表
+'''
+@csrf_exempt
+def index(request):
+    post = request.POST
+    param = {}
+    if request.method == "POST":
+        name = post.get('name')
+        number = post.get('number')
+        if name:
+            param.update(name={'$regex': name})
+        if number:
+            param.update(number={'$regex': number})
+    data = Mdset.objects.all().order_by("id")
+    limit = 20  # 每页显示的记录数
+    paginator = Paginator(data, limit)  # 实例化一个分页对象
+    page = request.GET.get('page')  # 获取页码
+    try:
+        topics = paginator.page(page)  # 获取某页对应的记录
+    except PageNotAnInteger:  # 如果页码不是个整数
+        topics = paginator.page(1)  # 取第一页的记录
+    except EmptyPage:  # 如果页码太大，没有相应的记录
+        topics = paginator.page(paginator.num_pages)  # 取最后一页的记录
+
+    return render(request, 'admin/mdset/index.html',{'list':list, 'request': post})
+
+ 
+ 
+# 添加操作--protected
+def _add(**param):
+    id = param.get('id')
+    password = param.get('password')
+    if not id:
+        if not password:
+            # 如果没有填密码则为默认密码
+            password = '123456'
+        param.update(password=make_password(password, None, 'pbkdf2_sha256'))
+        try:
+            model = Mdset(**param).add(**param)
+            if model:
+                returnData = {'code': '200', 'msg': '操作成功', 'data': str(model)}
+            else:
+                returnData = {'code': '801', 'msg': '操作失败', 'data': ''}
+        except Exception:
+            returnData = {'code': '900', 'msg': '数据验证错误', 'data': ''}
+    else:
+        returnData = {'code': '901', 'msg': '数据错误', 'data': ''}
+    return returnData
+
+# 修改操作--protected
+def _editById(**param):
+    id = param.get('id')
+    password = param.get('password')
+    if id:
+        if hasattr(param, 'password'):
+            if password:
+                param.update(password=make_password(password, None, 'pbkdf2_sha256'))
+            else:
+                # 如果留空则移除password属性，不修改密码
+                param.pop('password')
+        try:
+            model = Mdset(**param).editById(**param)
+            '''
+                返回值 model
+                nModified:修改成功1，修改失败0
+                updatedExisting:根据条件查询结果，有true，无false
+                n:根据条件查询结果，有1，无0
+                ok:1
+            '''
+            if model.get('n'):
+                if model.get('ok'):
+                    returnData = {'code': '200', 'msg': '操作成功', 'data': ''}
+                else:
+                    returnData = {'code': '801', 'msg': '操作失败', 'data': ''}
+            else:
+                returnData = {'code': '802', 'msg': '不存在的数据集', 'data': ''}
+        except Exception:
+            returnData = {'code': '900', 'msg': '数据验证错误', 'data': ''}
+    else:
+        returnData = {'code': '901', 'msg': '数据错误', 'data': ''}
+    return returnData
+
+# 修改操作
+@csrf_exempt
+def form(request):
+    post = request.POST
+    id = post.get('id')
+    param = {
+        'username': post.get('username'),
+        'nickname': post.get('nickname'),
+        'password': post.get('password'),
+        'email': post.get('email'),
+        'status': post.get('status'),
+    }
+    if id:
+        # 修改
+        param.update(id=id)
+        returnData = _editById(**param)
+    else:
+        # 添加
+        returnData = _add(**param)
+
+    return HttpResponse(json.dumps(returnData), content_type="application/json")
+
+# 更改状态操作
+@csrf_exempt
+def stats(request):
+    post = request.POST
+    selection = post.getlist('selection[]')
+    statusType = post.get('statusType')
+    if statusType == 'enable':
+        status = '1'
+    else:
+        status = '0'
+    param = {
+        'status': status,
+    }
+
+    try:
+        model = Mdset(**param).editByFilter(selection, **param)
+        if model.get('n'):
+            if model.get('ok'):
+                returnData = {'code': '200', 'msg': '操作成功', 'data': ''}
+            else:
+                returnData = {'code': '801', 'msg': '操作失败', 'data': ''}
+        else:
+            returnData = {'code': '802', 'msg': '不存在的数据集', 'data': ''}
+    except Exception:
+            returnData = {'code': '900', 'msg': '数据验证错误', 'data': ''}
+
+    return HttpResponse(json.dumps(returnData), content_type="application/json")
--- admin/urls.py
+++ admin/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from admin.controller import admin,accounts, contract
+from admin.controller import admin,accounts, contract, mdset
 urlpatterns = [
     url(r'^$', accounts.index, name='index'),
     url(r'^detail/$', admin.detail, name='detail'),
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^accounts/captcha', accounts.captcha,name='captcha'), #验证码
     url(r'^list/',admin.list, name='list'),#管理员列表
     url(r'^contract/index', contract.index, name='index'),
+    url(r'^mdset/index', mdset.index, name='index'),
     #url(r'^logout/', admin.logout, name='logout'),
 ]
