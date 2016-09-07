59da6d8a96a5e0d76a93e1f98e4dbf5fef95c2fc
zhaiyu <zhaiyu_hb@hotmail.com>
2016-09-07 15:39:52 +0800

    修改迈豆积分配置

--- admin/model/Mdset.py
+++ admin/model/Mdset.py
@@ -2,8 +2,9 @@
 from mongoengine import *
 from datetime import *
 import bson
-from django.conf import settings # import the settings file
+from django.conf import settings  # import the settings file
 from admin.model.Auth import Auth
+
 class Mdset(Auth):
     meta = Document.meta = {
             'collection': settings.MONGODB_PREFIX+'mdset',
