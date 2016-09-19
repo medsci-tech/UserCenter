bf232ba2f8487533629eba5cc5e02e5b7294c35b
fantasylxh <772932587@qq.com>
2016-09-19 16:20:19 +0800

    session 过期时间设置

--- UserCenter/settings.py
+++ UserCenter/settings.py
@@ -55,12 +55,12 @@ MIDDLEWARE = [
 
 SESSION_CACHE_ALIAS = 'default'                         # Cache to store session data if using the cache session backend.
 SESSION_COOKIE_NAME = 'sessionid'                       # Cookie name. This can be whatever you want.
-SESSION_COOKIE_AGE = 60 * 5                             # Age of cookie, in seconds (default: 2 weeks). Now is 5 minutes.
+SESSION_COOKIE_AGE = 60 * 5 * 24                             # Age of cookie, in seconds (default: 2 weeks). Now is 5 minutes.
 SESSION_COOKIE_DOMAIN = None                            # A string like ".example.com", or None for standard domain cookie.
 SESSION_COOKIE_SECURE = False                           # Whether the session cookie should be secure (https:// only).
 SESSION_COOKIE_PATH = '/'                               # The path of the session cookie.
 SESSION_COOKIE_HTTPONLY = True                          # Whether to use the non-RFC standard httpOnly flag (IE, FF3+, others)
-SESSION_SAVE_EVERY_REQUEST = False                      # Whether to save the session data on every request.
+SESSION_SAVE_EVERY_REQUEST = True                      # Whether to save the session data on every request.
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True                  # Whether a user's session cookie expires when the Web browser is closed.
 SESSION_ENGINE = 'django.contrib.sessions.backends.file'  # The module to store session data
 SESSION_FILE_PATH = None                                # Directory to store session files if using the file session module. If None, the backend will use a sensible default.
