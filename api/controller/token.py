0103046ae84286ff7a9e2f63cf84e2e4155df021
fantasylxh <772932587@qq.com>
2016-10-25 16:22:28 +0800

    完善注册接口

--- api/controller/token.py
+++ api/controller/token.py
@@ -2,13 +2,23 @@
 # token 加密方法
 from UserCenter.global_templates import configParam
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired
-
+from django.http import HttpResponse
+import time
+import datetime
 class QXToken(object):
     def __init__(self, name):
         self.name = name
         self.token_key = configParam().get('c_token_key')
 
     def generate_auth_token(self, expiration = 3600):
+        ticks = int(time.time())  # 当前时间戳
+        now = datetime.datetime.now()  # 获得当前时间
+        otherStyleTime = now.strftime("%Y-%m-%d")  # 转换为指定的格式:
+        endTime = otherStyleTime + ' 23:59:59'
+        timeArray = time.strptime(endTime, "%Y-%m-%d %H:%M:%S")
+        timeStamp = int(time.mktime(timeArray))  # 转换为时间戳
+        expiration = timeStamp - ticks # 设置当天有效期
+
         s = Serializer(self.token_key, expires_in=expiration)
         return str(s.dumps({'name': self.name}), encoding="utf-8")
 
@@ -17,10 +27,11 @@ class QXToken(object):
         try:
             data = s.loads(token)
         except SignatureExpired:
-            return None # valid token, but expired
+            return 0 # valid token, but expired
         except BadSignature:
-            return None # invalid token
+            return 0 # invalid token
         if data['name'] == self.name:
-            return self.name
+            return 200
+            #return self.name
         else:
-            return None
+            return 0
