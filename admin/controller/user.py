cc18d2e3db6ca35981715a96bbd01b8e04fc63ef
zhaiyu <zhaiyu_hb@hotmail.com>
2016-10-27 17:30:32 +0800

    用户管理加迈豆记录

--- admin/controller/user.py
+++ admin/controller/user.py
@@ -6,7 +6,8 @@ from admin.controller.common_import import *
 
 from django.contrib.auth.hashers import make_password
 from admin.model.User import User as Model
-
+from admin.model.Contract import Contract
+import datetime
 '''
 迈豆积分列表
 '''
@@ -27,6 +28,10 @@ def index(request):
     page = request.GET.get('page', 1)  # 获取页码
     pageData = paginationForMime(page=page, data=data)
 
+    try:
+        contratcData = Contract.objects.filter(status=1).order_by("id")
+    except:
+        contratcData = {}
     return render(request, 'admin/user/index.html', {
         'data_list': pageData.get('data_list'),
         'page_has_previous': pageData.get('pageLengthPrev'),
@@ -34,6 +39,7 @@ def index(request):
         'page_last': pageData.get('pageLast'),
         'page_range': range(pageData.get('pageStart'), pageData.get('pageEnd')),
         'ctrlList': post,
+        'list_contratcData': contratcData,
     })
 
 # 添加操作--protected
@@ -70,6 +76,7 @@ def _editById(**param):
                 # 如果留空则移除password属性，不修改密码
                 param.pop('password')
         try:
+            param.update(updated_at=datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
             model = Model.objects.get(id=id).update(**param)
             if model == 1:
                 returnData = {'code': '200', 'msg': '操作成功', 'data': ''}
@@ -89,14 +96,12 @@ def form(request):
         id = post.get('id')
         phone = post.get('phone')
         param = {
-            'nickname': post.get('nickname'),
             'phone': phone,
             'password': post.get('password'),
             'role': post.get('role'),
             'province': post.get('province'),
             'city': post.get('city'),
             'district': post.get('district'),
-            'status': post.get('status'),
         }
         try:
             model = Model.objects.get(phone=phone)
